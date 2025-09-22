a6a2587c0eda850e2f237403ffd8f020a87f29a8
SEGARS <Vim_Branet@mail.ru>
2025-09-22 10:26:51 +0500

    дз вектор, нужно прорабоать

---
+++ vector_02/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+
+//Нужно объяснение!
+int main()
+{
+	std::setlocale(LC_ALL, "ru");
+
+	std::cout << "Веддите количество элементов векторе - ";
+	int n;
+	std::cin >> n; //Указываем количество элементов векторе.
+	std::vector<int> alem(n);
+	
+	int min = 1;
+	int max = 0;
+
+	for (int i = 0; i < alem.size(); i++)
+	{
+		std::cin >> alem[i]; //Заполняем данными элементы вектора.		
+	}
+
+	for (int i = 0; i < alem.size(); i++)
+	{
+		if (alem[i] < min)
+		{
+			min = alem[i];
+		}
+		if(alem[i] > max)
+		{
+			max = alem[i];
+		}
+	}
+
+	for (int i = 0; i < alem.size(); i++)
+	{
+		std::cout << alem[i] << " ";
+	}
+
+	std::cout << std::endl;
+	std::cout << min << "\n";
+	std::cout << max;
+
+	return 0;
+}
