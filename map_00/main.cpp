8ec37b6cf9fd62cd7d7ac3e8fc8a842f7bc83afd
SEGARSS <vim_branet@mail.ru>
2025-09-30 17:49:17 +0500

    попкорн, продолжить с 8 видео

---
+++ map_00/main.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+
+
+struct Book
+{
+    std::string autor;
+    std::string name;
+};
+
+int main()
+{
+
+    //std::vector<int> v = { 9, 42, 777 };
+    //for (const int& item : v)
+    //{
+    //    std::cout << item;
+    //}
+
+    std::vector<Book> bibl;
+    bibl.push_back(Book({ "Auotor 2", "Kniga 2" }));
+    bibl.push_back(Book({ "Auotor 1", "Kniga 1" }));
+    bibl.push_back(Book({ "Auotor 2", "Kniga 3" }));
+
+    //bool found = false;
+
+    //for (const Book& tekBook : bibl)
+    //{
+    //    if (tekBook.autor == "Auotor 1")
+    //    {
+    //        std::cout << tekBook.name << std::endl;
+    //        found = true;
+    //        break;
+    //    }
+    //}
+
+    //if (found == false)
+    //{
+    //    std::cout << "not found!!" << std::endl;
+    //}
+
+    //for (const Book& tekBook : bibl)
+    //{
+    //    //Book& tekBook = bibl[i];
+    //    std::cout << tekBook.autor << " - " << tekBook.name << std::endl;
+    //}
+
+    std::map<std::string /*автор*/, std::vector<std::string> /*название*/> bibl_v2;
+    bibl_v2["Auotor 2"].push_back("Kniga 2");
+    bibl_v2["Auotor 1"].push_back("Kniga 1");
+    bibl_v2["Auotor 2"].push_back("Kniga 3");
+    bibl_v2["Auotor 2"].push_back("Kniga 4");
+    bibl_v2["Auotor 3"].push_back("Kniga 999");
+    bibl_v2["Auotor 3"].push_back("Kniga 7777");
+
+    std::vector<std::string> knigi_autora7 = { "kn555", "kn64556" };
+    bibl_v2["Auotor 7"] = knigi_autora7;
+
+    bibl_v2["Auotor 7"].push_back("kn555");
+    bibl_v2["Auotor 7"].push_back("kn64556");
+
+
+    //if (bibl_v2.contains("Auotor 1") == true)
+    //{
+    //    std::cout << bibl_v2["Auotor 1"] << std::endl;
+    //}
+    //else
+    //{
+    //    std::cout << "not found!!" << std::endl;
+    //}
+
+
+
+    //std::pair<std::string, std::vector<std::string>>&
+    for (auto& item : bibl_v2)
+    {
+        std::cout << item.first << " - " << std::endl;
+        for (const std::string& name : item.second)
+        {
+            std::cout << "   - " << name << std::endl;
+        }
+    }    
+
+    return 0;
+}
---
+++ map_01/main.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+/*
+
+    Book
+        - автор
+        - имя
+        - сколько страниц
+
+
+    сделать функцию createMap (создает библиотеку и добавляет туда 4 книги с разными авторами)
+
+    findAutor - сделать функцию, которая возвращает true или false и проверяет есть ли конкретный автор в нашей библиотеке
+
+    printAutorBooks - сделать функцию, которая ничего не возвращает, но выводит на экран названия книг и сколько в них страниц конкретного автора
+
+
+    */
+//---------------------------------------------------------------------------------------------------
+struct Book
+{
+    std::string autor;
+    std::string name;
+    int str;
+};
+//---------------------------------------------------------------------------------------------------
+std::map<std::string, std::vector<Book>> createMap()
+{
+    std::map<std::string, std::vector<Book>> bibl;
+    bibl["Книга_20"].push_back(Book({ "Писака", "История, ", 100 }));
+    bibl["Книга_10"].push_back(Book({ "Гуляка", "Поход, ", 200 }));
+    bibl["Книга_01"].push_back(Book({ "Леняка", "Лежакот, ", 300 }));
+    bibl["Книга_09"].push_back(Book({ "Пройдоха", "Пофигад, ", 400 }));
+
+    return bibl;
+}
+//---------------------------------------------------------------------------------------------------
+void printAutorBooks(std::map<std::string, std::vector<Book>> info)
+{
+    for (auto& item : info)
+    {
+        std::cout << item.first << std::endl;
+        for (const Book& buks : item.second)
+        {
+            std::cout << "   - " << buks.autor << " " << buks.name << " Количество странниц: - " << buks.str << std::endl;
+        }
+    }
+}
+//---------------------------------------------------------------------------------------------------
+bool findAutor(std::map<std::string, std::vector<Book>> info_, std::string autor)
+{
+    for (auto& item : info_)
+    {
+        for (const Book& buks : item.second)
+        {
+            if (buks.autor == autor)
+            {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+//---------------------------------------------------------------------------------------------------
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    std::map<std::string /*Номер книги*/, std::vector<Book> /*Данные об книжке*/> bibl = createMap();    
+
+    printAutorBooks(bibl);
+
+    bool find = findAutor(bibl, "Леняка");
+    if (find)
+    {
+        std::cout << "нашли\n";
+    }
+    else
+    {
+        std::cout << "НЕ нашли\n";
+    }
+
+    return 0;
+}
