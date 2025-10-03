c9669fcbfe94273be36a112d77e34a4756ea70d9
SEGARSS <vim_branet@mail.ru>
2025-10-03 14:55:46 +0500

    дз

---
+++ map_02/main.cpp
@@ -0,0 +1,90 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+
+
+/*Занятия.*/
+int main()
+{
+    // абвгде ... 
+    // 1 
+    // 
+    // абв -> бвг
+    // а -> б
+    // б -> в
+    // в -> г
+
+    // бвг -> абв
+
+
+    std::map<char /*оригинал*/, char /*шифр*/> cezar;
+    std::map<char /*шифр*/, char /*оригинал*/> deshifr;
+
+    int sdvig = 1;
+    // char  0 - 255
+    for (char c = 'a'; c <= 'z'; ++c)
+    {
+        //std::cout << c;
+        char c_2 = c + sdvig; // a -> b || b -> a
+        //std::cout << c_2;
+        //std::cout << std::endl;
+
+        cezar[c] = c_2;
+        deshifr[c_2] = c;
+    }
+
+    //for (auto& item : cezar)
+    //{
+    //    std::cout << item.first << item.second << std::endl;
+    //}
+
+    std::string msg = "hello123   !!!!";
+
+
+    for (auto c : msg)
+    {
+        if (cezar.contains(c))
+        {
+            std::cout << cezar[c];
+        }
+        else
+        {
+            std::cout << c;
+        }
+    }
+
+    std::cout << std::endl;
+
+    std::string shifr = "ifmmp123   !!!!";
+
+    for (auto c_2 : shifr)
+    {
+        if (deshifr.contains(c_2))
+        {
+            std::cout << deshifr[c_2];
+        }
+        else
+        {
+            std::cout << c_2;
+        }
+    }
+    
+
+
+
+    
+
+
+
+    // abcde......
+
+
+
+    // 1 доделать дефишратор - тебе нужно просто расшифровать shifr
+    
+
+
+
+    return 0;
+}
---
+++ map_03/main.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+
+// сделать ввод строки от пользователя - зашифровать эту строку - вывести зашифрованную 
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	std::map<char /*оригинал*/, char /*шифр*/> cezar;
+	std::map<char /*шифр*/, char /*оригинал*/> deshifr;
+
+	int sdvig = 1;
+
+	std::string vod;
+	std::string msg;
+
+	std::cout << "Ведите слово для зашифровки и расшифровки: - ";
+	std::cin >> vod;
+
+	for (char c = 'a'; c <= 'z'; c++)
+	{
+		char c_2 = c + sdvig;
+		cezar[c] = c_2;
+		deshifr[c_2] = c;
+	}
+
+	for (auto z : vod)
+	{
+		if (cezar.contains(z))
+		{
+			//std::cout << cezar[z];
+
+			//			
+			msg += cezar[z];
+			//std::cout << msg;			
+		}
+		else
+		{
+			//std::cout << z;
+
+			//std::string msg;			
+			msg += z;
+			//std::cout << msg;
+		}		
+	}
+
+	std::cout << msg;
+	std::cout << std::endl;
+
+	//std::cout << "Ведите слово для расшифровки: - ";
+	//std::cin >> vod;
+
+	for (auto r : vod)
+	{
+		if (deshifr.contains(r))
+		{
+			std::cout << deshifr[r];
+		}
+		else
+		{
+			std::cout << r;
+		}
+	}
+	return 0;
+}
